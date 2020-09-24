abfee5a8eeb38b3e66cc86c0dac29ff17191cfe2
Shihan Zhang <corazhang0@gmail.com>
2020-09-24 15:24:08 -0400

    Fix when input is not an number

--- script.js
+++ script.js
@@ -98,10 +98,12 @@ function PasswordTypeCheck() {
 
 function UserChoice() {
     var lengthChoice = prompt("Please choose the length of the password: ");
-    if (!Number.isInteger(lengthChoice)) {
+    //console.log(typeof lengthChoice);
+    if (isNaN(lengthChoice) === true) {
         alert("Please enter an integer");
         UserChoice();
         IsValid = false;
+
     }
     if (lengthChoice < 8 || lengthChoice > 128) {
         alert("The password must have at least 8 characters and no more than 128 characters");
